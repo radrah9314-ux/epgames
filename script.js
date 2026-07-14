c003951ef05eb08ed7609a11ec1a39396c5f31c3
radrah9314-ux <radrah.9314@gmail.com>
2026-07-14 22:43:18 +0330

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ const loginLink = document.querySelector(".login-link");
 const registerLink = document.querySelector(".Register-link");
 const btnPopup = document.querySelector(".btnLogin-popup");
 const iconClose = document.querySelectorAll(".icon-close");
+const loginForm = document.querySelector(".form-box.login form");
+const loginButton = document.querySelector(".btnLogin-popup");
+const profileMenu = document.querySelector(".profile-menu");
+const profileButton = document.querySelector(".btn-profile");
+const profileDropdown = document.querySelector(".profile-dropdown");
+const logout = document.querySelector("#logout");
+
 
 registerLink.addEventListener("click", ()=> {
     wrapper.classList.add("active");
@@ -22,3 +29,40 @@ iconClose.forEach(icon=> {
         wrapper.classList.remove("active");
     });
 });
+
+loginForm.addEventListener("submit", function (e) {
+    e.preventDefault(); // Prevent page refresh
+wrapper.classList.remove("active-popup");
+    wrapper.classList.remove("active");
+
+    // Hide Login button
+    loginButton.style.display = "none";
+
+    // Show Profile button
+    profileMenu.style.display = "block";
+});
+
+profileButton.addEventListener("click", function () {
+    if (profileDropdown.style.display === "block") {
+        profileDropdown.style.display = "none";
+    } else {
+        profileDropdown.style.display = "block";
+    }
+});
+
+logout.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    profileDropdown.style.display = "none";
+    profileMenu.style.display = "none";
+    loginButton.style.display = "inline-block";
+});
+
+function handleCredentialResponse(response) {
+    console.log(response);
+
+    wrapper.classList.remove("active-popup");
+
+    loginButton.style.display = "none";
+    profileMenu.style.display = "block";
+}
